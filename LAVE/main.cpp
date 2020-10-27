be5a0f51f035f9a04dc8a934d5b681fa6de203a0
Elmer <elmerr.vasquez@gmail.com>
2020-10-27 18:19:26 -0300

    Agrego txt y borro funciones de main
    Funciones.h y cpp siguen pero en breve se van

--- LAVE/main.cpp
+++ LAVE/main.cpp
@@ -7,15 +7,11 @@
 #include <string.h>
 #include <windows.h>
 #include <stdio.h>
-#include "funciones.h"
 #include "rlutil.h"
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Spanish");
-    gotoxy(50,1);
-    cout<<" L A V E \n";
-    login();
+
     return 0;
 }
