e7fe3a4f5e66d3f34ae7d4b1d7885a848e9c1d06
Elmer <elmerr.vasquez@gmail.com>
2020-10-27 20:25:56 -0300

    Prueba
    Prueba de rama

---
+++ LAVE/menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "usuario.h"
+
+class Menu{
+    protected:
+        Usuario u;
+        int nivel;
+        int rolDeUsuario;
+    public:
+    Menu();
+    ~Menu();
+    setNivel();
+
+};
+
+#endif // MENU_H_INCLUDED
